3fd65514161404e807d6a4798c2bcb87d6d95937
InasAdalia <adaliakhairudin@gmail.com>
2025-12-06 20:07:12 +0800

    added spotify api, but then spotify removed all previews

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,33 +7,6 @@ import { Icon } from '@iconify/react'
 function App() {
   const [res, setRes] = useState<string[]|''>('')
 
-  const fetchAPI = async () => {
-    try {
-      //this is normal fetch method
-      // const response : any = await fetch('http://localhost:8080/')
-      // const text = response.text()
-      // console.log(text)
-      // setRes(text)
-
-      //this is axios method
-      // const response = await axios.get('http://localhost:8080/api')
-      // const data = response.data
-
-      //this is axios and external api
-      console.log('test')
-      const url = 'https://againstlab.com/products.json?limit=10'
-      const response = await axios.get(url)
-      const data = response.data
-      console.log(data)
-      setRes(data.products)
-    } catch (error) {
-      
-    }
-  }
-
-  useEffect(()=>{
-    fetchAPI()
-  }, [])
 
   return (
     <div className="w-[375px] min-h-[95vh] mx-auto bg-white shadow-xl rounded-xl overflow-y-auto scrollbar-hide">
--- client/src/components/AudioUpload.tsx
+++ client/src/components/AudioUpload.tsx
@@ -1,18 +1,59 @@
 import { Icon } from '@iconify/react'
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 function AudioUpload() {
+
+    const [res, setRes] = useState<string[]|''>('');
+
+    useEffect(() => {
+        playSong();
+        fetchSomeSongs();
+    },[])
+
+    const fetchSomeSongs = async () => {
+    const response = await axios.get("http://localhost:8080/music/preview-tracks");
+        console.log(response.data); 
+        setRes(response.data);
+    }
+
+    const fetchSong = async () => {
+        const response = await axios.get("http://localhost:8080/music/drivers-license");
+        console.log(response.data);
+    };
+
+    const playSong = async () => {
+    const res = await axios.get("http://localhost:8080/music/drivers-license");
+    const preview = res.data.preview_url;
+
+    if (!preview) {
+        console.log("No preview available");
+        return;
+    }
+
+    const audio = new Audio(preview);
+        audio.play();
+    };
+
+
   return (
     <>
         <div className="flex justify-center items-center w-full ">
             <div
                 className="bg-gray-100 h-auto flex gap-1 justify-start items-space-between px-2 py-1 relative max-h-40 shadow-lg rounded-xl">
+                {/* ALBUM COVER */}
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs3pvw2oSLc9tpwVby2WnmtAgYLWP3tu3Lww&s" 
                     alt="album cover" className="rounded-2xl h-8 " />
+
+                {/* SONG TITLE */}
                 <p className="text-start text-xs font-semibold grow">
                     Roxette<br /><span className="text-xs font-normal">Sleeping in my car</span>
                 </p>
-                <span className="clear-left rounded-full bg-[#eff0f9] h-10 w-10 cursor-pointer flex items-center justify-center group">
+
+                {/* PLAY ICON */}
+                <span
+                    onClick={()=>{console.log(res)}} 
+                    className="clear-left rounded-full bg-[#eff0f9] h-10 w-10 cursor-pointer flex items-center justify-center group">
                 <span className="bg-white h-6 w-6 rounded-full shadow-md flex items-center justify-center group-hover:bg-rose-600">
                     <svg xmlns="http://www.w3.org/2000/svg" className="group-hover:fill-white group-hover:stroke-white" width="10" height="10" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#7e9cff" fill="#7e9cff" strokeLinecap="round" strokeLinejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
@@ -24,7 +65,9 @@ function AudioUpload() {
                 <Icon 
                     icon="mdi:add-circle" height="20" width="20" 
                     style={{ top: '50%', transform: 'translateY(-50%)' }}
-                    className="absolute right-[-1.5rem] cursor-pointer text-gray-500 hover:text-blue-600" />
+                    className="absolute right-[-1.5rem] cursor-pointer text-gray-500 hover:text-blue-600" 
+                    
+                    />
             </div>
         </div>
         {/* delete icon */}
